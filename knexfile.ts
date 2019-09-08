a9c705b315e95a3da540059e3fe1028e185d9133
e-e-e <benjamin.mforster@gmail.com>
2019-09-08 17:28:15 +1000

    user root on travis

--- knexfile.ts
+++ knexfile.ts
@@ -18,7 +18,7 @@ export default {
       host: '127.0.0.1',
       port: '3306',
       database: "slims_test",
-      user: 'travis',
+      user: 'root',
       password: "",
       multipleStatements: true
     },
